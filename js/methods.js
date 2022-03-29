58bc84d91a1f40e7db8e02e51058de1d19fbbb07
Arkashu <kopeikaarkashu@gmail.com>
2022-03-29 11:56:31 +0300

    update

--- js/methods.js
+++ js/methods.js
@@ -105,3 +105,23 @@
 //         arr.splice(i, 1)
 // }
 // console.log(arr)
+
+
+//Task#19
+// let arr = [1, 2, [3, 4], 5, [6, 7]];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     if (Array.isArray(arr[i])){
+//         null;
+//     } else {
+//         arr.splice(i, 1)}
+// }
+// console.log(arr)
+
+//Task#20
+// let arr = [5,-3, 6,-5, 0,-7, 8, 9];
+// let result = 0;
+// for (let i = 0; i <= arr.length-1; i++) {
+//     if (arr[i] <0)
+//         result +=1;
+// }
+// console.log(result)
